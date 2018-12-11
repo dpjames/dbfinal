35b87aa06c68501765958762061b20c494b19bb4
duxlow <daytonmux@yahoo.com>
2018-12-10 17:27:09 -0800

    Completed OR-1

--- src/Owner.java
+++ src/Owner.java
@@ -62,13 +62,15 @@ public class Owner {
       else {
 
          String query =
-            "SELECT ro0.RoomName, " +
+            "SELECT ro0.RoomName, ro0.RoomId," +
               "IF (0 = SUM(ddiff.ddd), 'Empty', " +
                 "IF (DATEDIFF(ADDDATE('" + dates[1] + "', INTERVAL 1 DAY), '" +
                      dates[0] + "') = SUM(ddiff.ddd), " +
                   "'Full', 'Partial')) 'Status' \n" +
             "FROM rooms ro0, \n" +
-               "(SELECT ro.RoomId, IF ('" + dates[0] + "' >= r.CheckOut OR r.CheckIn > '" + dates[1] + "', 0, DATEDIFF(IF(r.CheckOut > ADDDATE('" +
+               "(SELECT ro.RoomId, IF ('" + dates[0] + 
+               "' >= r.CheckOut OR r.CheckIn > '" + dates[1] +
+               "', 0, DATEDIFF(IF(r.CheckOut > ADDDATE('" +
                      dates[1] + "', INTERVAL 1 DAY), " + 
                                        "ADDDATE('" + dates[1] +
                                        "', INTERVAL 1 DAY), r.CheckOut), \n" +
@@ -87,8 +89,48 @@ public class Owner {
             System.out.println(e);
          }
 
+         String roomCode = InnReservations.getRoomCodeOrQ();
+         System.out.println(roomCode);
+         if (roomCode.equals("q")) {
+            return true;
+         }
+         query =
+            "SELECT Code, CheckIn, CheckOut \n" +
+            "FROM reservations \n" +
+            "WHERE Room = '" + roomCode + 
+               "' AND (CheckIn <= '" + dates[0] + 
+               "' AND CheckOut > '" + dates[1] +
+               "' OR  CheckIn > '" + dates[0] +
+               "' AND CheckIn <= '" + dates[1] +
+               "' OR  CheckOut > '" + dates[0] +
+               "' AND CheckOut <= '" + dates[1] +
+                "');";
 
-         return true;
+         results = Tables.doQuery(query, conn);
+         try{
+            Tables.prettyPrint(results);
+         }catch(SQLException e){
+            System.out.println(e);
+         }
+
+         String resCode = InnReservations.getReservCodeOrQ();
+         System.out.println(resCode);
+         if (resCode.equals("q")) {
+            return true;
+         }
+         query =
+            "SELECT *\n" +
+            "FROM reservations \n" +
+            "WHERE Code = " + resCode + ";";
+
+         results = Tables.doQuery(query, conn);
+         try{
+            Tables.prettyPrint(results);
+         }catch(SQLException e){
+            System.out.println(e);
+         }
+
+         return false;
       }
    }
 
